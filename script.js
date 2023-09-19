f2554673a89a7c6191bbd9ae4f1d35be5db6bc64
Thales Emerich <thales@lafine.com.br>
2023-09-19 14:08:55 -0300

    Alterado funcionamento Galeria

--- script.js
+++ script.js
@@ -14,41 +14,90 @@ window.addEventListener('scroll', function () {
 
 
 
-let currentImageIndex = 0;
-const images = document.querySelectorAll('.galeria-slide img');
-const ampliada = document.querySelector('.ampliada');
-const imagemAmpliada = document.getElementById('imagem-ampliada');
+
+
+
+
+
+let currentIndex = 0;
+const images = document.querySelectorAll('.slide-image');
+const modal = document.getElementById('imageModal');
+const modalImage = document.getElementById('modalImage');
 
 function showImage(index) {
-  if (index >= 0 && index < images.length) {
-    imagemAmpliada.src = images[index].src;
-    ampliada.style.display = 'block';
-    currentImageIndex = index;
-  }
-}
-
-function closeImage() {
-  ampliada.style.display = 'none';
+    images.forEach((image, i) => {
+        if (i === index) {
+            image.style.display = 'block';
+        } else {
+            image.style.display = 'none';
+        }
+    });
 }
 
 function prevImage() {
-  showImage(currentImageIndex - 1);
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
 }
 
 function nextImage() {
-  showImage(currentImageIndex + 1);
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
 }
 
-// Adicione um evento de clique a todas as imagens na galeria para ampliá-las
-images.forEach((image, index) => {
-  image.addEventListener('click', () => showImage(index));
+function openImage(index) {
+    modal.style.display = 'block';
+    modalImage.src = images[index].src;
+    currentIndex = index;
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+function prevModalImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    modalImage.src = images[currentIndex].src;
+}
+
+function nextModalImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    modalImage.src = images[currentIndex].src;
+}
+
+// Adicione um evento de tecla para sair do modal quando a tecla Esc for pressionada
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        closeModal();
+    } else if (event.key === 'ArrowLeft') {
+        prevModalImage();
+    } else if (event.key === 'ArrowRight') {
+        nextModalImage();
+    }
 });
 
-// Adicione um evento de clique à imagem ampliada para fechá-la
-ampliada.addEventListener('click', closeImage);
+// Adicione eventos de toque para dispositivos móveis
+let touchStartX = null;
 
-// Pré-carregue todas as imagens
-images.forEach((image) => {
-  const img = new Image();
-  img.src = image.src;
+modalImage.addEventListener('touchstart', (e) => {
+    touchStartX = e.touches[0].clientX;
 });
+
+modalImage.addEventListener('touchmove', (e) => {
+    if (touchStartX === null) return;
+
+    const touchEndX = e.touches[0].clientX;
+    const deltaX = touchEndX - touchStartX;
+
+    if (deltaX > 50) {
+        // Deslizou para a direita (avançar)
+        nextModalImage();
+    } else if (deltaX < -50) {
+        // Deslizou para a esquerda (retornar)
+        prevModalImage();
+    }
+
+    touchStartX = null;
+});
+
+// Inicialmente, exiba a primeira imagem
+showImage(currentIndex);
